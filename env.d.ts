9de808ecb84157ea8bec818c701a90544020bccc
Artem Marinin <aramarinin@beeline.ru>
2023-02-10 11:37:43 +0300

    add sass

--- env.d.ts
+++ env.d.ts
@@ -35,6 +35,10 @@ declare module '*.module.css' {
     const classes: { readonly [key: string]: string };
     export default classes;
 }
+declare module '*.scss' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
 
 type ObjectKeys<T> = T extends object
     ? (keyof T)[]
